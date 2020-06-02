fa62c24364b952814fa88f02dfa6434913d6d12f
Gabriel Cruz <gabriel.cruzjoa@gmail.com>
2020-06-01 20:57:01 -0400

    Update Ejercicios.cs

--- Ejercicios.cs
+++ Ejercicios.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,31 +8,34 @@ namespace PracticaLab
     {
         public void Ejercicio1() 
         {
-            int parN1 = 0, parN2 = 0, resultado = 1000;
-            int[] nums = { 21, 40, 59, 19, 31 };
+            int result = 1000; int par1 = 0, par2 = 0;
+            int[] nums = { 21, 16, 40, 59, 19};
 
             // Si el arreglo tiene un solo numero o ninguno debe retornar un -1
             if (nums.Length <= 1)
             {
-                resultado = -1;
-                Console.WriteLine("Resultado: {0}", resultado);
+                result = -1;
+                Console.WriteLine("Resultado: {0}", result);
             }
-            else 
+            else
             {
-                for (int i = 0; i < nums.Length - 1; i++) 
+                for (int i = 0; i < nums.Length - 1; i++)
                 {
-                    int j = i + 1;
-                    if (Math.Abs(nums[i] - nums[j]) < resultado)
+                    for (int j = i + 1; j < nums.Length; j++)
                     {
-                        parN1 = nums[i];
-                        parN2 = nums[j];
-                        resultado = Math.Abs(nums[i] - nums[j]);
+                        // Calculando resta con valor absoluto para obtener el par de numeros
+                        // cuya distancia sea la menor dentro del arreglo
+                        if (Math.Abs(nums[i] - nums[j]) < result)
+                        {
+                            par1 = nums[i];
+                            par2 = nums[j];
+                            result = Math.Abs(nums[i] - nums[j]);
+                        }
                     }
                 }
-                Console.WriteLine("El Par de numeros es: {0} y {1}", parN1, parN2);
-                Console.WriteLine("Resultado: {0}", resultado);
+                Console.WriteLine("El Par de numeros es: {0} y {1}", par1, par2);
+                Console.WriteLine("Resultado: {0}", result);
             }
         }
-
-    }
+    }    
 }
